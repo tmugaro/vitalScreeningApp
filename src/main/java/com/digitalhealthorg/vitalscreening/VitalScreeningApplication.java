004547ac690197ab73f0cd03bdb3fdddc8e6d0b7
tmugaro <naldotinashe1@gmail.com>
2023-07-14 10:05:07 -0400

    initial commit of the project structure

---
+++ src/main/java/com/digitalhealthorg/vitalscreening/VitalScreeningApplication.java
@@ -0,0 +1,13 @@
+package com.digitalhealthorg.vitalscreening;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VitalScreeningApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VitalScreeningApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/digitalhealthorg/vitalscreening/VitalScreeningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digitalhealthorg.vitalscreening;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VitalScreeningApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
