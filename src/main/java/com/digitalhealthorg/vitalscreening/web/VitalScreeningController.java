b645c4d10b0e4e15bfa4b1b36001c43d9847fca0
tmugaro <naldotinashe1@gmail.com>
2023-07-14 11:48:43 -0400

    added put controller to edit patients and vitals

--- src/main/java/com/digitalhealthorg/vitalscreening/web/VitalScreeningController.java
+++ src/main/java/com/digitalhealthorg/vitalscreening/web/VitalScreeningController.java
@@ -1,6 +1,7 @@
 package com.digitalhealthorg.vitalscreening.web;
 
 import com.digitalhealthorg.vitalscreening.entities.Patient;
+import com.digitalhealthorg.vitalscreening.repository.VitalScreeningRepository;
 import com.digitalhealthorg.vitalscreening.service.VitalSignsService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,9 +15,11 @@ public class VitalScreeningController {
 
         private VitalSignsService vitals;
 
-        @PostMapping("/savePatient")
+        private VitalScreeningRepository vitalScreeningRepository;
+
+        @PostMapping("/savePatientVitals")
         public ResponseEntity<String> savePatient(@RequestBody Patient patient) {
-            Integer id = vitals.savePatient(patient);
+            Patient id = vitals.savePatient(patient);
             return new ResponseEntity<String>("Patient with '" + id + "' has been saved", HttpStatus.OK);
         }
 
@@ -25,4 +28,25 @@ public class VitalScreeningController {
         List<Patient> list = vitals.getAllPatients();
         return new ResponseEntity<List<Patient>>(list,HttpStatus.OK);
     }
+
+    @PutMapping("/updatePatient/{id}")
+    public ResponseEntity<String> updatePatient( @PathVariable("id") Integer id,  @RequestBody Patient patient) {
+        Patient screening = vitalScreeningRepository.getPatientById(id);
+
+        screening.setFirstName(screening.getFirstName());
+        screening.setLastName(screening.getLastName());
+        screening.setDateOfBirth(screening.getDateOfBirth());
+        screening.setBloodGlucose(screening.getBloodGlucose());
+        screening.setHeight(screening.getHeight());
+        screening.setWeight(screening.getWeight());
+        screening.setDiastolicBloodPressure(screening.getDiastolicBloodPressure());
+        screening.setSystolicBloodPressure(screening.getSystolicBloodPressure());
+
+
+        screening = vitals.savePatient(screening);
+
+        return new ResponseEntity<String>("Patient with '" + id + "' has been updated", HttpStatus.OK);
+    }
+
+
 }
