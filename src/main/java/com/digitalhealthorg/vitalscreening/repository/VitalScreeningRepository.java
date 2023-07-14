cfcb6b3d1b808844b087b0d63d4f393c7961f194
tmugaro <naldotinashe1@gmail.com>
2023-07-14 10:59:25 -0400

    added a repository method

---
+++ src/main/java/com/digitalhealthorg/vitalscreening/repository/VitalScreeningRepository.java
@@ -0,0 +1,10 @@
+package com.digitalhealthorg.vitalscreening.repository;
+
+import com.digitalhealthorg.vitalscreening.entities.Patient;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface VitalScreeningRepository extends JpaRepository<Patient, Long> {
+
+}
